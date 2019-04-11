e7adc6e9e0fa1918990be849088ddefc6a1b86af
Liuchenxi <hpuer520@163.com>
2019-04-11 18:07:31 +0800

    明天开始调试接口

--- src/common/path.js
+++ src/common/path.js
@@ -1,4 +1,22 @@
 const path = {
-    'imagePath':'https://pictest.sqplus.cn/szjs/user/images/index/'
+    // 测试路径：
+	// 注意: 下面的start/end 标识用于自动化发布使用，不要删除
+    //  testenvstart
+    'imagePath':'https://pictest.sqplus.cn/szjs/user/images/index/',
+    'apiPath':'192.168.0.90:8089'
+    // testenvend
+
+    
+    // 预生产路径：
+    // //previewenvstart
+    // 'imagePath':'https://pictest.sqplus.cn/szjs/user/images/index/',
+    // 'apiPath':''
+   // //previewenvend
+
+    // 正式路径：
+    // //prodenvstart
+    
+    // //prodenvend
+
 };
 module.exports.path = path;
