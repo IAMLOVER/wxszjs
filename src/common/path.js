430cd965a674e4dfa4c061d5adf225e5ce41e86b
Liuchenxi <hpuer520@163.com>
2019-04-12 14:07:01 +0800

    第一个接口调试

--- src/common/path.js
+++ src/common/path.js
@@ -3,7 +3,7 @@ const path = {
 	// 注意: 下面的start/end 标识用于自动化发布使用，不要删除
     //  testenvstart
     'imagePath':'https://pictest.sqplus.cn/szjs/user/images/index/',
-    'apiPath':'192.168.0.90:8089'
+    'apiPath':'https://optest.sqplus.cn'
     // testenvend
 
     
