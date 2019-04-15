99eb831bc130141f7202149b67937411ce3a2144
Liuchenxi <hpuer520@163.com>
2019-04-15 20:42:56 +0800

    分享按钮

--- src/common/time.js
+++ src/common/time.js
@@ -1,14 +1,26 @@
-function date_time(val) {
-    var date = new Date(parseInt(val.replace("/Date(", "").replace(")/", ""), 10));
-    //月份为0-11，所以+1，月份小于10时补个0
-    var month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
-    var currentDate = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
-    var hour = date.getHours();
-    var minute = date.getMinutes();
-    var second = date.getSeconds();
-    var theTime = date.getFullYear() + "-" + month + "-" + currentDate + " " + hour + ":" + minute + ":" + second;
-    return theTime;
-}
-module.exports = {
-    date_time: date_time
+const formatNumber = n => {
+
+    n = n.toString()
+    
+    return n[1] ? n : '0' + n
+    }
+    
+    module.exports = {
+    formatTime: formatTime
+    }
+    
+    function formatTime(number, format) {
+    var formateArr = ['Y', 'M', 'D', 'h', 'm', 's'];
+    var returnArr = [];
+    var date = new Date(number * 1000);
+    returnArr.push(date.getFullYear());
+    returnArr.push(formatNumber(date.getMonth() + 1));
+    returnArr.push(formatNumber(date.getDate()));
+    returnArr.push(formatNumber(date.getHours()));
+    returnArr.push(formatNumber(date.getMinutes()));
+    returnArr.push(formatNumber(date.getSeconds()));
+    for (var i in returnArr) {
+    format = format.replace(formateArr[i], returnArr[i]);
+    }
+    return format;
 }
