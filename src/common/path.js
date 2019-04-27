1c3a91e55158b37377aaf004ce99cbf8ef1d7406
Liuchenxi <hpuer520@163.com>
2019-04-27 14:16:14 +0800

    修改图片路径

--- src/common/path.js
+++ src/common/path.js
@@ -2,8 +2,13 @@ const path = {
     // 测试路径：
 	// 注意: 下面的start/end 标识用于自动化发布使用，不要删除
     //  testenvstart
-    'imagePath':'https://pictest.sqplus.cn/szjs/user/images/index/',
-    'apiPath':'https://optest.sqplus.cn'
+    'imagePath':'https://picop.sqplus.cn/szjs/user/images/index/',
+    'apiPath':'https://op.sqplus.cn',
+
+    // 'imagePath':'https://pictest.sqplus.cn/szjs/user/images/index/',
+    // 'apiPath':'https://optest.sqplus.cn',
+    'preImagePath':'https://picop.sqplus.cn/sqj/',
+    // https://picop.sqplus.cn/sqj
     // testenvend
 
     
