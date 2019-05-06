faa63eda8be7c9296dea632607bde1c26547e718
Liuchenxi <hpuer520@163.com>
2019-05-06 18:47:46 +0800

    5.6号下午提交

--- src/common/path.js
+++ src/common/path.js
@@ -2,27 +2,16 @@ const path = {
     // 测试路径：
 	// 注意: 下面的start/end 标识用于自动化发布使用，不要删除
     //  testenvstart
+    //正式环境
     'imagePath':'https://picop.sqplus.cn/szjs/user/images/index/',
     'apiPath':'https://op.sqplus.cn',
     'preImagePath':'https://picop.sqplus.cn/sqj/',
 
-    // 'imagePath':'https://pictest.sqplus.cn/szjs/user/images/index/',
-    // 'apiPath':'https://optest.sqplus.cn',
-    // 'preImagePath':'https://pictest.sqplus.cn/sqj/',
-    // https://picop.sqplus.cn/sqj
+     //测试环境 
+    'imagePath':'https://pictest.sqplus.cn/szjs/user/images/index/',
+    'apiPath':'https://optest.sqplus.cn',
+    'preImagePath':'https://pictest.sqplus.cn/sqj/',
     // testenvend
-
-    
-    // 预生产路径：
-    // //previewenvstart
-    // 'imagePath':'https://pictest.sqplus.cn/szjs/user/images/index/',
-    // 'apiPath':''
-   // //previewenvend
-
-    // 正式路径：
-    // //prodenvstart
-    
-    // //prodenvend
 
 };
 module.exports.path = path;
